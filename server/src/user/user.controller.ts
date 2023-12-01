fabafd812e7c60271e3facbb79ff6c665d5aaf3c
viniciusvieirac <vini.clau0@gmail.com>
2023-12-01 11:34:05 -0300

    feat: create next app

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -2,11 +2,11 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserService } from './user.service';
 
-@Controller('signup')
+@Controller('login')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Post()
+  @Post('/signup')
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
   }

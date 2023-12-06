6ed45bafc7e5a2466732a89e54da454b07800f53
viniciusvieirac <vini.clau0@gmail.com>
2023-12-06 15:12:12 -0300

    feat: implement deploy database railway

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   dotenv.config();
 
   const app = await NestFactory.create(AppModule, { cors: true });
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 3001);
 }
 bootstrap();

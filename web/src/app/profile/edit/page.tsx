b08148f9019f2b3f7f068a01b412aaa0e1d42084
viniciusvieirac <vini.clau0@gmail.com>
2023-12-06 16:24:01 -0300

    fix: protected routes

--- web/src/app/profile/edit/page.tsx
+++ web/src/app/profile/edit/page.tsx
@@ -1,15 +1,15 @@
-'use client';
-import { updateUser } from '@/app/utils/AxiosFunctions';
-import { TextField } from '@mui/material';
-import Image from 'next/image';
-import { ChangeEvent, useContext, useState } from 'react';
+"use client";
+import { updateUser } from "@/app/utils/AxiosFunctions";
+import { TextField } from "@mui/material";
+import Image from "next/image";
+import { ChangeEvent, useContext, useState } from "react";
 import { useRouter } from "next/navigation";
-import AuthContext from '@/context/auth/AuthContext';
-
+import AuthContext from "@/context/auth/AuthContext";
+import ProtectedRoute from "@/components/ProtectedRoute";
 
 export default function EditProfile() {
   const router = useRouter();
-  const [description, setDescription] = useState('');
+  const [description, setDescription] = useState("");
   const { user, setUser } = useContext(AuthContext);
   const [selectedImage, setSelectedImage] = useState<string | null>(null);
 
@@ -23,61 +23,71 @@ export default function EditProfile() {
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const token = localStorage.getItem('token');
-    if(token){
-      await updateUser(token, { description, imageUrl: selectedImage?.toString() });
+    const token = localStorage.getItem("token");
+    if (token) {
+      await updateUser(token, {
+        description,
+        imageUrl: selectedImage?.toString(),
+      });
       setUser({
         ...user,
         description,
         imageUrl: selectedImage?.toString(),
       });
-      alert('Perfil atualizado com sucesso!');
-      router.push('/profile');
+      alert("Perfil atualizado com sucesso!");
+      router.push("/profile");
     }
   };
 
   return (
-    <div className="flex justify-center items-center h-screen">
-      <form onSubmit={handleSubmit} className="w-full max-w-md bg-white text-slate-900 rounded-lg shadow-lg p-6">
-        <h1 className="text-2xl font-bold mb-4">Perfil</h1>
-        {selectedImage && (
-          <div className="mb-4 text-center">
-            <Image
-              src={selectedImage}
-              alt="Imagem do perfil"
-              width={128}
-              height={128}
-              className="w-32 h-32 rounded-full mx-auto mb-2"
+    <ProtectedRoute>
+      <div className="flex justify-center items-center h-screen">
+        <form
+          onSubmit={handleSubmit}
+          className="w-full max-w-md bg-white text-slate-900 rounded-lg shadow-lg p-6"
+        >
+          <h1 className="text-2xl font-bold mb-4">Perfil</h1>
+          {selectedImage && (
+            <div className="mb-4 text-center">
+              <Image
+                src={selectedImage}
+                alt="Imagem do perfil"
+                width={128}
+                height={128}
+                className="w-32 h-32 rounded-full mx-auto mb-2"
+              />
+            </div>
+          )}
+          <div className="mb-4">
+            <TextField
+              id="filled-basic"
+              label="Adicione uma descrição"
+              variant="filled"
+              className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
             />
           </div>
-        )}
-        <div className="mb-4">
-          <TextField
-            id="filled-basic"
-            label="Adicione uma descrição"
-            variant="filled"
-            className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-          />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="image" className="block text-sm font-semibold mb-1">Imagem de Perfil</label>
-          <input
-            type="file"
-            id="image"
-            className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
-            onChange={handleImageChange}
-            accept="image/*"
-          />
-        </div>
-        <button
-          type="submit"
-          className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
-        >
-          Salvar
-        </button>
-      </form>
-    </div>
+          <div className="mb-4">
+            <label htmlFor="image" className="block text-sm font-semibold mb-1">
+              Imagem de Perfil
+            </label>
+            <input
+              type="file"
+              id="image"
+              className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
+              onChange={handleImageChange}
+              accept="image/*"
+            />
+          </div>
+          <button
+            type="submit"
+            className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
+          >
+            Salvar
+          </button>
+        </form>
+      </div>
+    </ProtectedRoute>
   );
 }
--- web/src/app/profile/page.tsx
+++ web/src/app/profile/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import ProtectedRoute from "@/components/ProtectedRoute";
 import AuthContext from "@/context/auth/AuthContext";
 import { Button } from "@mui/material";
 import Image from "next/image";
@@ -18,38 +19,40 @@ export default function Profile() {
   }, [getUser]);
 
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="w-full max-w-md bg-white text-slate-900 rounded-lg shadow-lg p-6">
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={() => router.push("/chat")}
-          className="w-full mb-4 bg-black"
-        >
-          Voltar
-        </Button>
-        <div className="mb-4 text-center">
-          {user.imageUrl && (
-            <Image
-              src={user.imageUrl}
-              alt="Imagem do perfil"
-              width={128}
-              height={128}
-              className="rounded-full mx-auto mb-2"
-            />
-          )}
+    <ProtectedRoute>
+      <div className="flex justify-center items-center h-screen">
+        <div className="w-full max-w-md bg-white text-slate-900 rounded-lg shadow-lg p-6">
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => router.push("/chat")}
+            className="w-full mb-4 bg-black"
+          >
+            Voltar
+          </Button>
+          <div className="mb-4 text-center">
+            {user.imageUrl && (
+              <Image
+                src={user.imageUrl}
+                alt="Imagem do perfil"
+                width={128}
+                height={128}
+                className="rounded-full mx-auto mb-2"
+              />
+            )}
+          </div>
+          <h1 className="text-2xl font-bold mb-4">{user.name}</h1>
+          <p className="text-sm mb-4">{user.description}</p>
+          <Button
+            variant="contained"
+            color="primary"
+            className="w-full mb-4 bg-black"
+            onClick={() => router.push("/profile/edit")}
+          >
+            Editar Perfil
+          </Button>
         </div>
-        <h1 className="text-2xl font-bold mb-4">{user.name}</h1>
-        <p className="text-sm mb-4">{user.description}</p>
-        <Button
-          variant="contained"
-          color="primary"
-          className="w-full mb-4 bg-black"
-          onClick={() => router.push("/profile/edit")}
-        >
-          Editar Perfil
-        </Button>
       </div>
-    </div>
+    </ProtectedRoute>
   );
 }
